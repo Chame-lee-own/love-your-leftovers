42718fafb1f5b2b6f03cad141a458e4661b6adfe
Chame-lee-own <camille.lo.x@gmail.com>
2017-03-20 17:43:30 +0000

    Update leftovers.py

--- leftovers.py
+++ leftovers.py
@@ -1,35 +1,39 @@
-import os
+
 from flask import Flask
 from flask import render_template
 from flask import request
 
+
 app = Flask("MyApp")
+
+
 
 @app.route("/welcome")
 def welcome():
     return render_template("leftover.html")
 
-@app.route("/<name>")
-def hello_someone(name):
-    return render_template("leftover.html", name=name.title())
+
+
+import requests
+
+endpoint = 'http://food2fork.com/api/search'
+payload = { 'key': 'a36d8f597d70a074e4866e5c25ff8a6c', 'q':
+'first ingredient, second ingredient', 'sort': 'r'}
+
+endpoint1 = 'http://food2fork.com/api/get'
+payload2 = { 'key': 'a36d8f597d70a074e4866e5c25ff8a6c', 'rId': ''}
+
+response = requests.get(endpoint, params = payload)
 
 
 
-@app.route("/bye/<name>")
-def bye_someone(name):
-    return render_template("leftover.html", bye=True, name=name.title())
 
-@app.route("/goodbye/<name>/<time>")
-def bye_custom(name, time):
-    return render_template("leftover.html", bye=True, name=name.title())
+@app.route("/search", methods=['POST'])
+def index():
+    results = requests.get(endpoint, params = payload)
+    return render_template('results.html', results=results)
 
 
-@app.route("/signup", methods=['POST'])
-def sign_up():
-    form_data = request.form
-    print form_data['first ingredient']
-    print form_data['second ingredient']
-    return "That's submitted!"
 
 
 
